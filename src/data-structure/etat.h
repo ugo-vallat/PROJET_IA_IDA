83556a4b47c07145f4603d4a9b6c9d911ea04dee
Enzo M <98408955+Zerkome@users.noreply.github.com>
2023-11-15 19:56:10 +0100

    Add files via upload
    etat.h finis

--- src/data-structure/etat.h
+++ src/data-structure/etat.h
@@ -1,16 +1,28 @@
-
-
 #ifndef __ETAT_H__
 #define __ETAT_H__
-
+#include <utils.h>
 
 typedef struct s_etat {
-    int matrice[4][4];
-} Etat;
+  int matrice[4][4];
+}Etat;
 
-typedef struct s_etat* ptrEtat;
+typedef struct s_operationPossibles{
+  Couple operations;
+  Etat etatFils
+  int couts;
+}operationPossibles;
 
+void AfficherEtat(Etat e);
 
+Couple* TrouverMouvements(Etat e);
+
+Etat Deplacer(Etat e,int pique_depart, int pique_arrivee);
+
+bool estBut(Etat e);
+
+bool  egal(Etat e, Etat but);
+
+operationPossibles opPoss(Etat e);
 
 
 
