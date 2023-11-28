2b76bc4d466c4ce8f3225f4d685930c2db67512b
Enzo M <98408955+Zerkome@users.noreply.github.com>
2023-11-28 13:02:25 +0100

    fichier de avec les datas pour faires les tests

---
+++ src/data-structure/dataTest.h
@@ -0,0 +1,16 @@
+#ifndef __DATATEST_H__
+#define __DATATEST_H__
+#include "utils.h"
+
+
+/**
+ * @brief Mets à jour les états fournis pour qu'ils correspondent au jeux de test voulu
+ * 
+ * @param id Numéro du jeu de test
+ * @param ed Nom de l'état initial
+ * @param ef Nom de l'état final
+ */
+void getEtatTest(int id, State *ed, State *ef);
+
+
+#endif
